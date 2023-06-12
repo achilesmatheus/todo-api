b309dbd333fb3bb90b79f45c8a1089cfb799ceca
Achiles <achiles1707@gmail.com>
2023-06-12 18:54:28 -0300

    feat: Created Http methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,8 +6,32 @@ namespace Todo.Controllers;
 public class HomeController : ControllerBase
 {
     [HttpGet("/")]
-    public string Get()
+    public IActionResult Get()
     {
-        return "";
+        return Ok("");
+    }
+
+    [HttpGet("/{id:int}")]
+    public IActionResult GetById()
+    {
+        return Ok("");
+    }
+
+    [HttpPost("/")]
+    public IActionResult Create()
+    {
+        return Ok("");
+    }
+
+    [HttpPut("/{id:int}")]
+    public IActionResult Update()
+    {
+        return Ok("");
+    }
+
+    [HttpDelete("/{id:int}")]
+    public IActionResult Delete()
+    {
+        return Ok("");
     }
 }
