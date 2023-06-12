ed59fe778c465500811f4164989764d06ed3f124
Achiles <achiles1707@gmail.com>
2023-06-12 19:13:41 -0300

    change: Removed others httl methods from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,28 +16,4 @@ public class HomeController : ControllerBase
             Online = true
         });
     }
-
-    [HttpGet("/{id:int}")]
-    public IActionResult GetById()
-    {
-        return Ok("");
-    }
-
-    [HttpPost("/")]
-    public IActionResult Create()
-    {
-        return Ok("");
-    }
-
-    [HttpPut("/{id:int}")]
-    public IActionResult Update()
-    {
-        return Ok("");
-    }
-
-    [HttpDelete("/{id:int}")]
-    public IActionResult Delete()
-    {
-        return Ok("");
-    }
 }
