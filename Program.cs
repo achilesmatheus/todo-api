9b67923ce4f1b6cfc4cd133bdbf2d7b4c731edaa
Achiles <achiles1707@gmail.com>
2023-06-13 17:16:33 -0300

    change: Register TokenService for DI

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Todo.Data;
+using Todo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
