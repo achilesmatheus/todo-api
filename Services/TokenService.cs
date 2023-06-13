2e7df8d5b65803bca26be37cf6b0a77f27042aea
Achiles <achiles1707@gmail.com>
2023-06-13 17:14:33 -0300

    feat: Created TokenService

---
+++ Services/TokenService.cs
@@ -0,0 +1,27 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Todo.Configurations;
+
+namespace Todo.Services;
+
+public class TokenService
+{
+    public string GenerateToken(string user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(JwtConfiguration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(6),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature
+            )
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
