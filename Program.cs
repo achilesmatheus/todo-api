832f320154bd0a3938fe23773eb466d57319ec40
Achiles <achiles1707@gmail.com>
2023-06-14 23:19:44 -0300

    change: Added config to avoid object cycle error

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Options;
 using Todo.Data;
+using Todo.Repositories;
 using Todo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddTransient<TokenService>();
+builder.Services.AddTransient<TaskRepository>();
 
 var app = builder.Build();
 
