0943c6ec921ecf33670d8d9a080875ec8f23910b
Achiles <achiles1707@gmail.com>
2023-06-15 23:28:24 -0300

    feat: Created task repository

---
+++ Repositories/TaskRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+using Todo.Models;
+using Todo.Repositories.Contracts;
+
+namespace Todo.Repositories;
+
+public class TaskRepository : GenericRepository<TaskModel>, ITaskRepository<TaskModel>
+{
+    private readonly AppDbContext _context;
+    public TaskRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<TaskModel>> GetByPeriodAsync(DateTime date)
+    {
+        return await _context.Set<TaskModel>()
+            .Where(x => x.CreatedAt.Date == date.Date)
+            .ToListAsync();
+    }
+}
