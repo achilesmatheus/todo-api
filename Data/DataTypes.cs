f257f1bce87cb6c8772299258d3b3f9e04322cb1
Achiles <achiles1707@gmail.com>
2023-06-15 23:22:46 -0300

    feat: Created constants of sql data types

---
+++ Data/DataTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Data;
+
+public static class SqlDataTypes
+{
+    public const string NVARCHAR = "nvarchar";
+    public const string DATETIME2 = "datetime2";
+    public const string BIT = "bit";
+    public const string INT = "int";
+}
