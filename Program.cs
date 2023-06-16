e53b277bc59914ebb214608cb4553fb648d07223
Achiles <achiles1707@gmail.com>
2023-06-16 20:22:40 -0300

    change: JsonOptions is no longer needed

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ using Todo.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    });
+    .AddControllers();
+// .AddJsonOptions(options =>
+// {
+//     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+// });
 
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddTransient<TokenService>();
 builder.Services.AddTransient<TaskRepository>();
 builder.Services.AddTransient<ListRepository>();
+builder.Services.AddTransient<FolderRepository>();
 
 var app = builder.Build();
 
