b1629a66edbbda59b193a85133e263e6ce14c634
Achiles <achiles1707@gmail.com>
2023-06-15 23:25:18 -0300

    feat: Created generic repository interface

---
+++ Repositories/Contracts/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Repositories.Contracts;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetFirstAsync();
+    Task<IEnumerable<T>> GetAllAsync();
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+}
