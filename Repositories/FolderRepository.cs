1b0ae2f51bdb7081f5aac456ac2080e5cb566966
Achiles <achiles1707@gmail.com>
2023-06-15 23:28:55 -0300

    feat: Created folder repository

---
+++ Repositories/FolderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Repositories;
+
+public class FolderRepository : GenericRepository<TaskModel>
+{
+    public FolderRepository(AppDbContext context) : base(context)
+    {
+    }
+}
