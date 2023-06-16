972c8eb80e42f1e73f0cc851a91360b3a2022be4
Achiles <achiles1707@gmail.com>
2023-06-16 20:26:00 -0300

    change: Modified const name and value

--- Data/DataTypes.cs
+++ Data/DataTypes.cs
@@ -3,7 +3,7 @@
 public static class SqlDataTypes
 {
     public const string NVARCHAR = "nvarchar";
-    public const string DATETIME2 = "datetime2";
+    public const string DATETIME = "datetime";
     public const string BIT = "bit";
     public const string INT = "int";
 }
