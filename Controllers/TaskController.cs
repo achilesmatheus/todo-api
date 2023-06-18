8d97209977b5608ecc60a3822acd4e8717400b88
Achiles <achiles1707@gmail.com>
2023-06-18 19:11:33 -0300

    change: Added interfaces dependency in Controllers

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.Models;
 using Todo.Repositories;
+using Todo.Repositories.Contracts;
 using Todo.ViewModels;
 
 namespace Todo.Controllers;
@@ -12,7 +13,7 @@ public class TaskController : ControllerBase
 {
     [HttpGet("tasks")]
     public async Task<IActionResult> GetAll(
-        [FromServices] TaskRepository repository
+        [FromServices] ITaskRepository repository
     )
     {
         try
@@ -31,7 +32,7 @@ public class TaskController : ControllerBase
 
     [HttpGet("task/{id:int}")]
     public async Task<IActionResult> GetById(
-        [FromServices] TaskRepository repository,
+        [FromServices] ITaskRepository repository,
         [FromRoute] int id
     )
     {
@@ -57,7 +58,7 @@ public class TaskController : ControllerBase
 
     [HttpGet("tasks/{date:datetime}")]
     public async Task<IActionResult> GetByPeriod(
-        [FromServices] TaskRepository repository,
+        [FromServices] ITaskRepository repository,
         [FromRoute] DateTime date
     )
     {
@@ -84,7 +85,7 @@ public class TaskController : ControllerBase
 
     [HttpPost("task")]
     public async Task<IActionResult> CreateTask(
-        [FromServices] TaskRepository taskRepository,
+        [FromServices] ITaskRepository repository,
         [FromBody] TaskViewModel model
     )
     {
@@ -94,11 +95,9 @@ public class TaskController : ControllerBase
             {
                 Title = model.Title,
                 Description = model.Description,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now,
             };
 
-            await taskRepository.CreateAsync(task);
+            await repository.CreateAsync(task);
 
             var result = new ResultViewModel<TaskModel>(task);
             return StatusCode(201, result);
@@ -120,21 +119,21 @@ public class TaskController : ControllerBase
 
     [HttpPut("task/{id:int}")]
     public async Task<IActionResult> UpdateTask(
-        [FromServices] TaskRepository taskRepository,
+        [FromServices] ITaskRepository repository,
         [FromBody] TaskViewModel viewModel,
         [FromRoute] int id
     )
     {
         try
         {
-            var task = await taskRepository.GetByIdAsync(id);
+            var task = await repository.GetByIdAsync(id);
             if (task == null) throw new DbUpdateException($"The task with id {id} was not found");
 
             task.Title = viewModel.Title;
             task.Description = viewModel.Description;
             task.UpdatedAt = DateTime.Now;
 
-            await taskRepository.UpdateAsync(task);
+            await repository.UpdateAsync(task);
 
             var result = new ResultViewModel<TaskModel>(task);
             return StatusCode(200, result);
@@ -153,18 +152,18 @@ public class TaskController : ControllerBase
 
     [HttpPut("task/{id:int}/mark-as-done")]
     public async Task<IActionResult> MarkAsDone(
-        [FromServices] TaskRepository taskRepository,
+        [FromServices] ITaskRepository repository,
         [FromRoute] int id
     )
     {
         try
         {
-            var task = await taskRepository.GetByIdAsync(id);
+            var task = await repository.GetByIdAsync(id);
             if (task == null) throw new DbUpdateException($"The task with id {id} was not found");
 
             task.Done = true;
             task.UpdatedAt = DateTime.Now;
-            await taskRepository.UpdateAsync(task);
+            await repository.UpdateAsync(task);
 
             var result = new ResultViewModel<TaskModel>(task);
             return StatusCode(200, result);
@@ -183,18 +182,18 @@ public class TaskController : ControllerBase
 
     [HttpPut("task/{id:int}/mark-as-undone")]
     public async Task<IActionResult> MarkAsUnDone(
-        [FromServices] TaskRepository taskRepository,
+        [FromServices] ITaskRepository repository,
         [FromRoute] int id
     )
     {
         try
         {
-            var task = await taskRepository.GetByIdAsync(id);
+            var task = await repository.GetByIdAsync(id);
             if (task == null) throw new DbUpdateException($"The task with id {id} was not found");
 
             task.Done = false;
             task.UpdatedAt = DateTime.Now;
-            await taskRepository.UpdateAsync(task);
+            await repository.UpdateAsync(task);
 
             var result = new ResultViewModel<TaskModel>(task);
             return StatusCode(200, result);
@@ -213,8 +212,8 @@ public class TaskController : ControllerBase
 
     [HttpPut("task/{taskId:int}/list/{listId:int}")]
     public async Task<IActionResult> MoveTaskToList(
-        [FromServices] TaskRepository taskRepository,
-        [FromServices] ListRepository listRepository,
+        [FromServices] TaskRepository repository,
+        [FromServices] IListRepository listRepository,
         [FromRoute] int taskId,
         [FromRoute] int listId
 
@@ -222,14 +221,42 @@ public class TaskController : ControllerBase
     {
         try
         {
-            var task = await taskRepository.GetByIdAsync(taskId);
+            var task = await repository.GetByIdAsync(taskId);
             var list = await listRepository.GetByIdAsync(listId);
 
             if (task == null) throw new DbUpdateException($"Task with id '{listId}' could not be found");
             if (list == null) throw new DbUpdateException($"List with id '{taskId}' could not be found");
 
             list.AddTask(task);
-            await taskRepository.UpdateAsync(task);
+            await repository.UpdateAsync(task);
+
+            var result = new ResultViewModel<TaskModel>(task);
+            return StatusCode(200, result);
+        }
+        catch (DbUpdateException ex)
+        {
+            var result = new ResultViewModel<string>(ex.Message);
+            return StatusCode(404, result);
+        }
+        catch
+        {
+            var result = new ResultViewModel<string>("Internal server error");
+            return StatusCode(500, result);
+        }
+    }
+
+    [HttpDelete("task/{id:int}")]
+    public async Task<IActionResult> DeleteAsync(
+    [FromServices] ITaskRepository repository,
+    [FromRoute] int id
+)
+    {
+        try
+        {
+            var task = await repository.GetByIdAsync(id);
+            if (task == null) throw new DbUpdateException($"The task with id '{id}' does not exist");
+
+            await repository.DeleteAsync(task);
 
             var result = new ResultViewModel<TaskModel>(task);
             return StatusCode(200, result);
