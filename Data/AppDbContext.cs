c687fa705a93bde5d8317cd213b6e1c582e30dc2
Achiles <achiles1707@gmail.com>
2023-06-17 21:11:18 -0300

    change: Added config for SqlServer and config for Mappings

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Todo.Data.FluentMapping;
 using Todo.Models;
 
 namespace Todo.Data;
 
 public class AppDbContext : DbContext
+
 {
+    private const string CONNECTION_STRING = "Server=localhost;Database=Todo;User ID=sa;Password=1425;Trust Server Certificate=true";
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     public DbSet<TaskModel> Tasks { get; set; }
     public DbSet<ListModel> Lists { get; set; }
     public DbSet<FolderModel> Folders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseSqlite("DataSource=app.db;Cache=Shared");
+        options.UseSqlServer(CONNECTION_STRING);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TaskMap());
+        modelBuilder.ApplyConfiguration(new ListMap());
+        modelBuilder.ApplyConfiguration(new FolderMap());
     }
 }
