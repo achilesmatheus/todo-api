65627135ed9e12766b50c6dcccc9ebb032f23563
Achiles <achiles1707@gmail.com>
2023-06-17 21:41:06 -0300

    feat: Created FolderMap

---
+++ Data/FluentMapping/FolderMap.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Todo.Models;
+
+namespace Todo.Data.FluentMapping;
+
+public class FolderMap : IEntityTypeConfiguration<FolderModel>
+{
+    public void Configure(EntityTypeBuilder<FolderModel> builder)
+    {
+        builder.ToTable("Folder");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Name)
+            .HasColumnType(SqlDataTypes.NVARCHAR)
+            .HasMaxLength(80);
+
+        builder.HasIndex(x => x.Name)
+            .IsUnique();
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType(SqlDataTypes.DATETIME2)
+            .HasDefaultValue(DateTime.UtcNow);
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnType(SqlDataTypes.DATETIME2)
+            .HasDefaultValue(DateTime.UtcNow);
+
+        builder.HasMany(x => x.Lists)
+            .WithOne("FolderId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
