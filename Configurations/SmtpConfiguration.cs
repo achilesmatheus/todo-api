adcf43e35986db32c72190f143b32c377d7c26b5
Achiles <achiles1707@gmail.com>
2023-06-28 18:14:36 -0300

    change: Added smtp config

---
+++ Configurations/SmtpConfiguration.cs
@@ -0,0 +1,9 @@
+namespace Todo.Configurations;
+
+public static class SmtpConfiguration
+{
+    public static int Port { get; set; }
+    public static string Host { get; set; }
+    public static string Username { get; set; }
+    public static string Password { get; set; }
+}
