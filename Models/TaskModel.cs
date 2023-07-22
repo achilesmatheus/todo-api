0fe05c3ee8ebc06ec12439cdc5bca3c891bc0a38
Achiles <achiles1707@gmail.com>
2023-07-22 13:14:01 -0300

    change: Property Description From required to optional

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -1,11 +1,13 @@
-﻿namespace Todo.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Todo.Models;
 
 public class TaskModel
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public bool Done { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
