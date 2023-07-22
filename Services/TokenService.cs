3539a6c54c274deb86611b99b50a2b0e94b17a11
Achiles <achiles1707@gmail.com>
2023-07-22 13:17:42 -0300

    change: Created GetClaims method

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,18 +1,21 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Todo.Configurations;
+using Todo.Models;
 
 namespace Todo.Services;
 
 public class TokenService
 {
-    public string GenerateToken(string user)
+    public string GenerateToken(UserModel user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(JwtConfiguration.JwtKey);
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(GetClaims(user)),
             Expires = DateTime.UtcNow.AddHours(6),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),
@@ -23,5 +26,17 @@ public class TokenService
         var token = tokenHandler.CreateToken(tokenDescriptor);
 
         return tokenHandler.WriteToken(token);
+    }
+
+    public IEnumerable<Claim> GetClaims(UserModel user)
+    {
+        var result = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.Email)
+        };
+        result.AddRange(
+            user.Roles.Select(role => new Claim(ClaimTypes.Role, role.Name))
+        );
+        return result;
     }
 }
