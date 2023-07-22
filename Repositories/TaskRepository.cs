8a6e58f7cc8728159884b1dd64743540dddd7ce8
Achiles <achiles1707@gmail.com>
2023-07-22 13:16:18 -0300

    change: Added a custom method called GetAllWithFilters

--- Repositories/TaskRepository.cs
+++ Repositories/TaskRepository.cs
@@ -5,12 +5,22 @@ using Todo.Repositories.Contracts;
 
 namespace Todo.Repositories;
 
-public class TaskRepository : GenericRepository<TaskModel>, ITaskRepository<TaskModel>
+public class TaskRepository : GenericRepository<TaskModel>, ITaskRepository
 {
     private readonly AppDbContext _context;
     public TaskRepository(AppDbContext context) : base(context)
     {
         _context = context;
+    }
+
+    public async Task<IEnumerable<TaskModel>> GetAllWithFilterAsync(int skip, int take)
+    {
+        return await _context.Tasks
+            .AsNoTracking()
+            .Skip(skip * take)
+            .Take(take)
+            .OrderBy(x => x.Id)
+            .ToListAsync();
     }
 
     public async Task<IEnumerable<TaskModel>> GetByPeriodAsync(DateTime date)
