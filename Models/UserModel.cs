f3bce61be59fa469d791177b3877d454b1b96f9b
Achiles <achiles1707@gmail.com>
2023-07-22 13:14:46 -0300

    change: Added missing properties

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -3,4 +3,11 @@
 public class UserModel
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public IList<RoleModel> Roles { get; set; }
 }
