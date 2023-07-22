2c4c5c7af923d6b2f4ad8909809747af44e8ad17
Achiles <achiles1707@gmail.com>
2023-07-22 13:12:32 -0300

    change: Removed unnecessary OnConfiguring method

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,23 +7,15 @@ namespace Todo.Data;
 public class AppDbContext : DbContext
 
 {
-    private const string CONNECTION_STRING = "Server=localhost;Database=Todo;User ID=sa;Password=1425;Trust Server Certificate=true";
-
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<TaskModel> Tasks { get; set; }
-    public DbSet<ListModel> Lists { get; set; }
-    public DbSet<FolderModel> Folders { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlServer(CONNECTION_STRING);
-    }
+    public DbSet<UserModel> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new TaskMap());
-        modelBuilder.ApplyConfiguration(new ListMap());
-        modelBuilder.ApplyConfiguration(new FolderMap());
+        modelBuilder.ApplyConfiguration(new UserMap());
+        modelBuilder.ApplyConfiguration(new RoleMap());
     }
 }
