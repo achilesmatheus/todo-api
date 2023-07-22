5741e011c773ad0e8468d41c2df72bb93de894dc
Achiles <achiles1707@gmail.com>
2023-07-22 13:19:50 -0300

    change: Property Title is now Required

--- ViewModels/TaskViewModel.cs
+++ ViewModels/TaskViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Todo.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.ViewModels;
 
 public class TaskViewModel
 {
+    [Required]
     public string Title { get; set; }
+
     public string Description { get; set; }
 }
