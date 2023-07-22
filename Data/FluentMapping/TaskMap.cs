f9ebea57fed547234b178f97b645f4444fbedd7b
Achiles <achiles1707@gmail.com>
2023-07-22 13:12:50 -0300

    change: Removed column name

--- Data/FluentMapping/TaskMap.cs
+++ Data/FluentMapping/TaskMap.cs
@@ -13,40 +13,29 @@ public class TaskMap : IEntityTypeConfiguration<TaskModel>
         builder.HasKey(t => t.Id);
 
         builder.Property(t => t.Id)
-            .HasColumnName("id")
             .ValueGeneratedOnAdd();
 
         builder.Property(t => t.Title)
             .IsRequired()
-            .HasColumnName("title")
             .HasColumnType(SqlDataTypes.NVARCHAR)
             .HasMaxLength(80);
 
         builder.Property(t => t.Done)
             .IsRequired()
-            .HasColumnName("done")
             .HasColumnType(SqlDataTypes.BIT)
             .HasDefaultValue(false);
 
         builder.Property(t => t.Description)
             .IsRequired()
-            .HasColumnName("description")
             .HasColumnType(SqlDataTypes.NVARCHAR)
             .HasMaxLength(250);
 
         builder.Property(t => t.CreatedAt)
-            .HasColumnName("created_at")
             .HasColumnType(SqlDataTypes.DATETIME2)
-            .HasDefaultValue(DateTime.Now.ToUniversalTime());
+            .HasDefaultValueSql("GETUTCDATE()");
 
         builder.Property(t => t.UpdatedAt)
-            .HasColumnName("updated_at")
             .HasColumnType(SqlDataTypes.DATETIME2)
-            .HasDefaultValue(DateTime.Now.ToUniversalTime());
-
-        // Relationships
-        builder.HasOne(t => t.List)
-            .WithMany(x => x.Tasks)
-            .HasConstraintName("FK_Task_List");
+            .HasDefaultValueSql("GETUTCDATE()");
     }
 }
