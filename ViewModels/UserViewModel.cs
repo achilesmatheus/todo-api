d0dcd93dfd3e93fd42fe8d5286b78ea22f526833
Achiles <achiles1707@gmail.com>
2023-07-22 13:20:03 -0300

    feat: Created UserViewModel

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.ViewModels;
+
+public class UserViewModel
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
